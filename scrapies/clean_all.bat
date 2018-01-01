6fd6b27716877f36e2f2c933b1fc0844a8f09fe3
Pierre-Lefevre <lefevre.pierre.m.d@gmail.com>
2018-01-01 15:49:50 +0100

    Transforms the batch clean script to a bash script and adds bash script to load all json files in the mongodb database.

--- scrapies/clean_all.bat
+++
@@ -1,8 +0,0 @@
-call clean_store.bat auchan
-call clean_store.bat boulanger
-call clean_store.bat cdiscount
-call clean_store.bat darty
-call clean_store.bat fnac
-call clean_store.bat ldlc
-call clean_store.bat materiel_net
-call clean_store.bat rue_du_commerce
--- scrapies/clean_store.bat
+++
@@ -1,5 +0,0 @@
-@RD /S /Q "C:\Users\Pierre\Desktop\INeedDisAtDisPrice\scrapies\data\%1\img"
-mkdir "C:\Users\Pierre\Desktop\INeedDisAtDisPrice\scrapies\data\%1\img"
-
-@RD /S /Q "C:\Users\Pierre\Desktop\INeedDisAtDisPrice\scrapies\data\%1\json"
-mkdir "C:\Users\Pierre\Desktop\INeedDisAtDisPrice\scrapies\data\%1\json"
