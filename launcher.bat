4373919e4d89131b0b39d6a36f8cb6321a277bcd
Pierre-Lefevre <lefevre.pierre.m.d@gmail.com>
2018-01-09 09:13:48 +0100

    Add a launcher batch script.

---
+++ launcher.bat
@@ -0,0 +1,6 @@
+start cmd /c "mongod"
+cd C:\Users\Pierre\Desktop\INeedDisAtDisPrice\app\client
+start cmd /c "npm run start-w"
+start cmd /c "npm run start"
+cd C:\Users\Pierre\Desktop\INeedDisAtDisPrice\app\server
+start cmd /c "npm run start"
